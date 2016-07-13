03e72ba12027c883e63dc5d322bd1bb8e6e6fe46
Tergiver <tergiver@msn.com>
2016-07-13 08:10:21 -0700

    Updated output text
    Changed output text to use localized name and include the total amount
    of resource removed in brackets

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A Factorio mod that allows you to remove ore and other resources from the world.
 
 To use it, craft an Ore Eraser, found on the Production tab. It functions like a Blueprint or Deconstruction Planner. With the Ore Eraser in hand, click and drag a rectangle encompassing the resource you want to remove.
 
+v1.2.0
+  - Changed output text to use localized name and include the total amount of resource removed in brackets
+
 v1.1.0
   - Changed output text so that it lists the counts of each resource removed.
   - Changed selection parameters so that the selection box is green and it highlights resources in green. There many items that highlight in green, but these are not removed, only entities defined as type 'resource'. It's unfortunate that you have limited control over the highlighting of a selection-tool. This change still makes it clearer which resource nodes will be removed.
--- control.lua
+++ control.lua
@@ -4,28 +4,33 @@ script.on_event(defines.events.on_player_selected_area, function(event)
 		local item = event.item
 		local entities = event.entities
 		if item == "ore-eraser" then
-			local destroyed_counts = {}
+			local destroyed_info = {}
 			for _,entity in ipairs(entities) do
 				if entity.type == "resource" then
-					if destroyed_counts[entity.name] == nil then
-						destroyed_counts[entity.name] = 1
+					local name = entity.name
+					if destroyed_info[name] == nil then
+						destroyed_info[name] = { count = 1, amount = entity.amount, localised_name = entity.localised_name[1] }
 					else
-						destroyed_counts[entity.name] = destroyed_counts[entity.name] + 1
+						destroyed_info[name].count = destroyed_info[name].count + 1
+						destroyed_info[name].amount = destroyed_info[name].amount + entity.amount
 					end
 					entity.destroy()
 				end
 			end
-			local text = ""
-			for k,v in pairs(destroyed_counts) do
-				if #text == 0 then
+			local args = {""}
+			for k,v in pairs(destroyed_info) do
+				local text = ""
+				if #args == 1 then
 					text = "Resources removed: "
 				else
 					text = text .. ", "
 				end
-				text = text .. v .. " " .. k
+				table.insert(args, text .. v.count .. " ")
+				table.insert(args, {v.localised_name})
+				table.insert(args, " [" .. v.amount .. "]")
 			end
-			if #text > 0 then
-				player.print(text)
+			if #args > 1 then
+				player.print(args)
 			end
 		end
 	end)

ace4ac682df776e83c0016b8c3beb6272cdd1049
wutname1 <jeremy12@gmail.com>
2019-03-03 02:24:27 -0600

    Update ore-eraser.lua
    Removed recipe & moved to quickbar

--- prototypes/ore-eraser.lua
+++ prototypes/ore-eraser.lua
@@ -1,30 +1,52 @@
 data:extend(
-{
-  {
-    type = "selection-tool",
-    name = "ore-eraser",
-    icon = "__ore-eraser__/graphics/ore-eraser.png",
-    flags = {"goes-to-quickbar"},
-    subgroup = "tool",
-    order = "c[automated-construction]-b[deconstruction-planner]",
-    stack_size = 1,
-	icon_size = 32,
-	stackable = false,
-    selection_color = { r = 0, g = 1, b = 0 },
-    alt_selection_color = { r = 0, g = 0, b = 1 },
-    selection_mode = {"any-entity"},
-    alt_selection_mode = {"any-entity"},
-    selection_cursor_box_type = "pair",
-    alt_selection_cursor_box_type = "pair"
-  },
-  {
-	type = "recipe",
-	name = "ore-eraser",
-	enabled = "true",
-	ingredients =
 	{
-	},
-	result = "ore-eraser"
-  }
-}
+		{
+			type = 'selection-tool',
+			name = 'ore-eraser',
+			icon = '__ore-eraser__/graphics/ore-eraser.png',
+			subgroup = 'tool',
+			flags = {'hidden'},
+			order = 'c[automated-construction]-b[deconstruction-planner]',
+			stack_size = 1,
+			icon_size = 32,
+			stackable = false,
+			selection_color = {r = 0, g = 1, b = 0},
+			alt_selection_color = {r = 0, g = 0, b = 1},
+			selection_mode = {'any-entity'},
+			alt_selection_mode = {'any-entity'},
+			selection_cursor_box_type = 'pair',
+			alt_selection_cursor_box_type = 'pair'
+		},
+		{
+			type = 'shortcut',
+			name = 'give-ore-eraser',
+			order = 'c[automated-construction]-b[deconstruction-planner]',
+			action = 'create-blueprint-item',
+			localised_name = {'item-name.ore-eraser'},
+			technology_to_unlock = 'construction-robotics',
+			item_to_create = 'ore-eraser',
+			style = 'red',
+			icon = {
+				filename = '__ore-eraser__/graphics/ore-eraser.png',
+				priority = 'extra-high-no-scale',
+				size = 32,
+				scale = 1,
+				flags = {'icon'}
+			},
+			small_icon = {
+				filename = '__ore-eraser__/graphics/ore-eraser.png',
+				priority = 'extra-high-no-scale',
+				size = 24,
+				scale = 1,
+				flags = {'icon'}
+			},
+			disabled_small_icon = {
+				filename = '__ore-eraser__/graphics/ore-eraser.png',
+				priority = 'extra-high-no-scale',
+				size = 24,
+				scale = 1,
+				flags = {'icon'}
+			}
+		}
+	}
 )
